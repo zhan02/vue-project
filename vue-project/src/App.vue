2d0cdbcdbceeece74f665609065e9e0c7b727b32
zhanhuixian <zhanhuixian@qq.com>
2022-03-26 18:54:31 +0800

    second chenecheripick

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -17,8 +17,8 @@ import HelloWorld from "@/components/HelloWorld.vue";
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">HomeCity</RouterLink>
+        <RouterLink to="/about">AboutChange</RouterLink>
       </nav>
     </div>
   </header>
